c2e8151c4acfe810e2fe44d2e4fb303ce8af08da
张高境 <491308652@qq.com>
2023-06-07 10:44:19 +0800

    1

--- 名片管理系统/cards_tools.py
+++ 名片管理系统/cards_tools.py
@@ -47,6 +47,30 @@ def show_all():
     """显示所有名片"""
     print("-" * 50)
     print("显示所有名片")
+    
+    # 判断是否存在名片记录，如果没有，提示用户返回
+    if len(card_list) == 0:
+
+        print("当前没有记录，请添加！")
+        
+        # return 可以返回一个函数执行结果，下方的代码不会被执行
+        # 如果return后面没有任何的内容，表示会返回到调用函数的位置，
+        # 并且不返回任何结果
+        return 
+    
+    # 打印表头
+    for name in["姓名","电话","QQ","邮箱"]:
+        print(name,end="\t\t") 
+    print("")
+
+    ## 打印分割线
+    print("=" * 50)
+    # 遍历名片列表依次输出字典消息
+    for card_dict in card_list:
+        print("%s\t\t%s\t\t%s\t\t%s" %(card_dict["name"],
+                                       card_dict["phone"],
+                                       card_dict["qq"],
+                                       card_dict["email"],))
 
 def search_card():
 
@@ -54,3 +78,24 @@ def search_card():
     print("-" * 50)
     print("搜索名片")
 
+    # 1. 提示用户输入要搜索的姓名
+    find_name = input("请输入要搜索的姓名：")
+    # 2. 遍历名片列表，查询要搜索的姓名，如果没有找到，需要提示用户
+    for card_dict in card_list:
+        if card_dict["name"] == find_name:
+
+            #print("找到了")
+            print("姓名\t\t电话\t\tQQ\t\t邮箱")
+            ## 打印分割线
+            print("=" * 50)
+            # 遍历名片列表依次输出字典消息
+            for card_dict in card_list:
+                print("%s\t\t%s\t\t%s\t\t%s" %(card_dict["name"],
+                                            card_dict["phone"],
+                                            card_dict["qq"],
+                                            card_dict["email"],))
+            # TODO 针对找到的名片记录执行修改和删除的操作
+            break
+
+    else: 
+        print("没有找到 %s" %find_name)
